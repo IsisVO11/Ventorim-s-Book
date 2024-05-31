4df0bbf37007075661ca7c86b02459789be16ae2
Isis Ventorim <110046234+IsisVO11@users.noreply.github.com>
2024-05-31 15:13:16 +0100

    Update shop_db.sql

--- shop_db.sql
+++ shop_db.sql
@@ -3,19 +3,19 @@
 -- Table structure for table `cart`
 
 CREATE TABLE `cart` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `price` int(100) NOT NULL,
-  `quantity` int(100) NOT NULL,
+  `price` int NOT NULL,
+  `quantity` int NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `message`
 
 CREATE TABLE `message` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
@@ -26,15 +26,15 @@ CREATE TABLE `message` (
 -- Table structure for table `orders`
 
 CREATE TABLE `orders` (
-  `id` int(100) NOT NULL,
-  `user_id` int(100) NOT NULL,
+  `id` int NOT NULL,
+  `user_id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `number` varchar(12) NOT NULL,
   `email` varchar(100) NOT NULL,
   `method` varchar(50) NOT NULL,
   `address` varchar(500) NOT NULL,
   `total_products` varchar(1000) NOT NULL,
-  `total_price` int(100) NOT NULL,
+  `total_price` int NOT NULL,
   `placed_on` varchar(50) NOT NULL,
   `payment_status` varchar(20) NOT NULL DEFAULT 'pending'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -42,25 +42,22 @@ CREATE TABLE `orders` (
 -- Table structure for table `products`
 
 CREATE TABLE `products` (
-  `id` int(100) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
-  `price` int(100) NOT NULL,
+  `price` int NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- Table structure for table `users`
 
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
+  `id` int NOT NULL,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
   `user_type` varchar(20) NOT NULL DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-ALTER TABLE `products`
-ADD COLUMN `genre` varchar(50) NOT NULL;
-
 ALTER TABLE `cart`
   ADD PRIMARY KEY (`id`);
 
@@ -75,19 +72,24 @@ ALTER TABLE `products`
 
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
+  
+ALTER TABLE `products`
+ADD COLUMN `genre` varchar(50) NOT NULL;
 
 ALTER TABLE `cart`
   MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 ALTER TABLE `message`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 ALTER TABLE `orders`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  ALTER TABLE `orders`
+ADD COLUMN quantity int NOT NULL DEFAULT 0;
 
 ALTER TABLE `products`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 ALTER TABLE `users`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 COMMIT;
